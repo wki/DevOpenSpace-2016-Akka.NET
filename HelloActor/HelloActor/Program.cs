c1a889ecc8d0d501af3113580ce80bb3d3571418
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2016-07-15 18:52:56 +0200

    added HelloActor

---
+++ HelloActor/HelloActor/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Akka.Actor;
+
+namespace HelloActor
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("HelloActorSystem");
+
+            // TODO: Aktor vom Typ "HelloActor" erzeugen
+            //       Hinweis: Props.Create<>() als Konstruktor-Ersatz
+
+            // TODO: dem Aktor eine String Nachricht "Hello" senden
+
+            Console.WriteLine("Press [enter] to stop");
+            Console.ReadLine();
+
+            system.Terminate();
+        }
+    }
+
+    public class HelloActor : ReceiveActor
+    {
+        public HelloActor()
+        {
+            Receive<string>(message => 
+                Console.WriteLine(message)
+            );
+
+            Receive<object>(message =>
+                Console.WriteLine("OOps: got a {0}: {1}", 
+                    message.GetType().FullName, message
+                )
+            );
+        }
+
+        protected override void Unhandled(object message)
+        {
+            Console.WriteLine("OOps: got a {0}: {1}", 
+                message.GetType().FullName, message
+            );
+        }
+    }
+}
