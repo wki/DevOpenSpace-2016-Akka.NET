6ea20063ed1a4ca157204e2eb8e0437e42701632
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2016-08-01 21:09:33 +0200

    updated packages, added remote actor

---
+++ Remote/SimpleClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Configuration;
+
+namespace SimpleClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("Client");
+
+        }
+    }
+}
---
+++ Remote/SimpleServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Configuration;
+
+namespace SimpleServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("Server", ConfigurationFactory.Load());
+
+            var helloActor = system.ActorOf(Props.Create<HelloActor>(), "hello");
+
+            Console.WriteLine("Waiting for remote calls. Press [enter] to stop");
+            Console.ReadLine();
+
+            system.Terminate().Wait();
+        }
+    }
+
+    public class HelloActor : ReceiveActor
+    {
+        public HelloActor()
+        {
+            Receive<string>(s =>
+                Console.WriteLine($"String '{s}' received from {Sender}")
+            );
+        }
+    }
+}
